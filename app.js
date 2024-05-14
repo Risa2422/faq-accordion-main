2f8c63da3be3c23ad53429926a79da0dbc16b2fc
Risa Yamamoto <yamari2422@gmail.com>
2024-05-14 09:09:39 -0700

    added app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const p = document.querySelectorAll(".button");
+const a = document.querySelectorAll(".answer");
+
+p.forEach((button, index) => {
+  button.addEventListener("click", function () {
+    a[index].classList.toggle("hide");
+  });
+});
