6dc469884ed841807b958c7936d047b8972e0311
Risa Yamamoto <yamari2422@gmail.com>
2024-05-15 15:05:16 -0700

    commited for now

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const a = document.querySelectorAll(".answer");
 
 p.forEach((button, index) => {
   button.addEventListener("click", function () {
-    a[index].classList.toggle("hide");
+    a[index].classList.toggle("show");
+    this.classList.toggle("visible");
   });
 });
